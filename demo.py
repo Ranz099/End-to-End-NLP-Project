f2fab7826e330cc8a41be7856e44b5c16bacca29
Ranjan Kumar <aranjan8172@gmail.com>
2024-12-20 11:19:14 +0530

    initial commit

---
+++ demo.py
@@ -0,0 +1,13 @@
+from hate.logger import logging
+from hate.exception import CustomException
+import sys
+from hate.configuration.gcloud_syncer import GCloudSync
+
+
+
+
+
+
+
+obj=GCloudSync()
+obj.sync_folder_from_gcloud("hate-classification","dataset.zip","dataset.zip")
