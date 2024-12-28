8a346986b53d351c674ddabd8f91f56d11c68002
Ranjan Kumar <aranjan8172@gmail.com>
2024-12-28 08:43:03 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages, setup
 setup(
     name="hate-speech-classification",
     version="0.0.1",
-    author="AYUSH",
-    author_email="ayushkumarshaw770@gmail.com",
+    author="RANJAN",
+    author_email="aranjan8172@gmail.com",
     packages=find_packages(),
     install_requires=[],
 )
